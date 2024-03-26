c114a778d2b6780d4375069f2d6bd48efc3634ae
Adrien <adrien.bouteiller01@gmail.com>
2024-03-26 16:25:53 +0100

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: false,
 
   // add base url sur nuxt 3 for production
   // https://v3.nuxtjs.org/docs/3.x/configuration-glossary/configuration-router
 
   app: {
-    baseURL: "/rocket/adrien/movieNuxt/",
+    baseURL: "/",
     buildAssetsDir: "/_nuxt/",
     cdnURL: "",
   },
