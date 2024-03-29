976b5202d9d37d29b98cc5f3337bc1eebefa890e
Adrien <adrien.bouteiller01@gmail.com>
2024-03-29 17:29:17 +0100

    up

--- pages/Movies/index.vue
+++ pages/Movies/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
+  <div class="MoviesList">
     <!-- create 4 grid tailwind to show images backdrop -->
     <div class="grid grid-cols-4 gap-4">
       <div class="movies" v-for="m in movies.results" @click="goToMovie(m.id)">
         <img
-          :src="`https://image.tmdb.org/t/p/w1280/${m.backdrop_path}`"
+          :src="`https://image.tmdb.org/t/p/w500/${m.poster_path}`"
           alt=""
+          width="200"
+          height="auto"
         />
       </div>
     </div>
@@ -18,10 +20,20 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 
 const { data: movies } = await useFetch("/api/movies?page=2&language=fr");
-console.log(movies.value);
 const goToMovie = (id) => {
   router.push(`/movies/${id}`);
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.MoviesList {
+  padding: 15px;
+  background-color: #111111;
+}
+
+.movies {
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+}
+</style>
